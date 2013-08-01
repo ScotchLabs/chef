453b89716d99eb8efb305a66300a45f8e0c7de3f
Carlos Diaz-Padron <carlos@carlosdp.io>
2013-07-31 17:29:59 -0700

    Add ruby and nginx-passenger

--- Berksfile
+++ Berksfile
@@ -4,4 +4,5 @@ cookbook "nginx", "~> 0.101.5"
 cookbook "redisio"
 cookbook "postfix"
 cookbook "mysql"
+cookbook "ruby"
 
--- roles/scotch.rb
+++ roles/scotch.rb
@@ -1,6 +1,6 @@
 name "scotch"
 description "Configuration for the Scotch server"
-run_list "recipe[mysql::server]", "recipe[redisio]", "recipe[nginx]", "recipe[postfix]"
+run_list "recipe[mysql::server]", "recipe[redisio]", "recipe[ruby]", "recipe[nginx]", "recipe[nginx::passenger]", "recipe[postfix]"
 override_attributes({
   "starter_name" => "Scotch Labs",
 })
